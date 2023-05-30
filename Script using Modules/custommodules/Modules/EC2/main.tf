5abe017ea6e6448b2b53513f74e75e1d58b95254
Simranjeet Singh <97332641+simranjeetsingh27@users.noreply.github.com>
2023-05-30 12:33:44 +0530

    Update main.tf

--- Script using Modules/custommodules/Modules/EC2/main.tf
+++ Script using Modules/custommodules/Modules/EC2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Bastin" {
   ami = "ami-0a940cb939351ccca"
   instance_type = "t2.small"
-  vpc_security_group_ids = [var.sg-basion-id]
+  vpc_security_group_ids = [var.sg-bashion-id]
   subnet_id = "var.subnet_id[*]"
   key_name = "N_virgina"
   associate_public_ip_address = true
