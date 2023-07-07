0fe394e51eb982a5d4ea33a2cc4b530d88f21f3b
Simranjeet Singh <97332641+simranjeetsingh27@users.noreply.github.com>
2023-07-07 13:50:34 +0530

    Update main.tf

--- Script using Modules/custommodules/Modules/EC2/main.tf
+++ Script using Modules/custommodules/Modules/EC2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Bastion" {
   instance_type = "t2.small"
   vpc_security_group_ids = [var.sg-bashion-id]
   subnet_id = "var.subnet_id[*]"
-  key_name = "N_virgina"
+  key_name = "n_virgina"
   associate_public_ip_address = true
   tags = {
     Name = "bastion"
